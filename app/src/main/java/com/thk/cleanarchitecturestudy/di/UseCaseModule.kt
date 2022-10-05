129b4c812cacd8e94744840087395d7443cb02dd
Kang Taehee <dev.thk28@gmail.com>
2022-10-05 11:13:08 +0900

    Hilt 관련 변경
    * domain 모듈에서 Hilt 제거
    * UseCase hilt 모듈을 app 모듈로 이동

--- app/src/main/java/com/thk/cleanarchitecturestudy/di/UseCaseModule.kt
+++ app/src/main/java/com/thk/cleanarchitecturestudy/di/UseCaseModule.kt
@@ -1,4 +1,4 @@
-package com.thk.domain.di
+package com.thk.cleanarchitecturestudy.di
 
 import com.thk.domain.repository.NumbersRepository
 import com.thk.domain.usecase.ClearNumbersUseCase
@@ -7,21 +7,20 @@ import com.thk.domain.usecase.InsertNumberUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ViewModelComponent::class)
 object UseCaseModule {
-    @Singleton
     @Provides
     fun provideGetNumbersUseCase(repository: NumbersRepository) = GetNumbersUseCase(repository)
 
-    @Singleton
     @Provides
     fun provideInsertNumberUseCase(repository: NumbersRepository) = InsertNumberUseCase(repository)
 
-    @Singleton
     @Provides
     fun provideClearNumbersUseCase(repository: NumbersRepository) = ClearNumbersUseCase(repository)
 }
--- domain/src/main/java/com/thk/domain/usecase/UseCases.kt
+++ domain/src/main/java/com/thk/domain/usecase/UseCases.kt
@@ -3,26 +3,25 @@ package com.thk.domain.usecase
 import androidx.lifecycle.LiveData
 import com.thk.domain.model.NumberModel
 import com.thk.domain.repository.NumbersRepository
-import javax.inject.Inject
 
 /*
     각 기능 별 UseCase 클래스 정의 
  */
 
-class GetNumbersUseCase @Inject constructor(private val repository: NumbersRepository) {
+class GetNumbersUseCase(private val repository: NumbersRepository) {
     operator fun invoke(): LiveData<List<NumberModel>> {
         return repository.getNumbers()
     }
 }
 
-class InsertNumberUseCase @Inject constructor(private val repository: NumbersRepository) {
+class InsertNumberUseCase(private val repository: NumbersRepository) {
     suspend operator fun invoke(value: Int) {
         val number = NumberModel(id = 0, value = value)
         repository.insertNumber(number)
     }
 }
 
-class ClearNumbersUseCase @Inject constructor(private val repository: NumbersRepository) {
+class ClearNumbersUseCase(private val repository: NumbersRepository) {
     suspend operator fun invoke() {
         repository.clearNumbers()
     }
